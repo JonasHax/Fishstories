669dc7d4217c450a54e951001e69b68f58073d25
Jonas Haxholm <jonashaxholm@hotmail.dk>
2021-05-12 14:15:49 +0200

    Make CatchReport description clickable and fresh fade also ;-)

--- src/Components/CatchReport/BottomBar.js
+++ src/Components/CatchReport/BottomBar.js
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "./BottomBar.module.css";
 import FishType from "../../images/fish.png";
 import Weight from "../../images/weight.png";
 import Ruler from "../../images/ruler.png";
 
 export const BottomBar = (props) => {
+  const [toggled, setToggled] = useState(false);
+
+  const handleClick = () => {
+    setToggled(!toggled);
+  };
+
   return (
-    <div className={css.BottomBarContainer}>
+    <div
+      className={[
+        css.BottomBarContainer,
+        toggled ? css.ExpandedDescription : null,
+      ].join(" ")}
+      onClick={handleClick}
+    >
       <div className="FishType">
-        <img src={FishType} alt="Art" className={css.FishIcon}></img>
+        <img src={FishType} alt="Art" className={css.FishIcon}></img>{" "}
         {props.fishType}
       </div>
       <div className={css.Weight}>
-        <img src={Weight} alt="Vægt" className={css.Icons}></img>
-        {props.weight} kg
+        <img src={Weight} alt="Vægt" className={css.Icons}></img> {props.weight}{" "}
+        kg
       </div>
       <div className={css.Lenght}>
-        <img src={Ruler} alt="Længde" className={css.Icons}></img>
+        <img src={Ruler} alt="Længde" className={css.Icons}></img>{" "}
         {props.lenght} cm
       </div>
       <div className={css.Description}>{props.description}</div>
+      <div
+        className={
+          !toggled && props.description.length > 100 ? css.TextFade : null
+        }
+      />
     </div>
   );
 };
